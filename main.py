875394d7cde72ac05e4ea3d3e034e4df1b087cdf
AlexUrluescu <urluescualex2003@gmail.com>
2023-04-08 17:23:05 +0300

    main: Organize the code

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ print("branch development")
 print("test")
 
 import gui
+import button
 import sys
 from PyQt6.QtWidgets import QApplication
 
 def main():
-    app = QApplication(sys.argv)
-    window = gui.Gui(500,500, "Vocal Home Automation")
-    window.build()
-    app.exec()
     print("Hello")
 
 
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+    window = gui.Gui(500,500, "Vocal Home Automation")
+    window.build()
+    sys.exit(app.exec())
