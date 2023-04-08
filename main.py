1f6c424886b62bf85716a8b52bab6ed9c4f1474c
AlexUrluescu <urluescualex2003@gmail.com>
2023-04-08 11:34:16 +0300

    main: Implement the Gui Class()

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-print("Hello world")
-
 print("branch development")
+
+import gui
+import sys
+from PyQt6.QtWidgets import QApplication
+
+def main():
+    app = QApplication(sys.argv)
+    window = gui.Gui(500,500, "Vocal Home Automation")
+    window.build()
+    app.exec()
+    print("Hello")
+
+
+if __name__ == "__main__":
+    main()
