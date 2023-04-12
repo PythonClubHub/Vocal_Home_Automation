2643432016f2cffb428dcde0e012a2aebf75fdf4
Timothee <timotei.sandru2022@gmail.com>
2023-04-12 22:14:59 +0300

    Add read sensor example

---
+++ src/dht22.py
@@ -0,0 +1,13 @@
+import Adafruit_DHT
+
+dht_sensor = Adafruit_DHT.DHT22
+
+dht_pin = 4
+
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(dht_sensor, dht_pin)
+
+    if humidity is not None and temperature is not None:
+        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+    else:
+        print("Failed to retreive data from humidity sensor")
