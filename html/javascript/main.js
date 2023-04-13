33d356d14c4e67055c76cb78590d8413b5ebbc9d
AlexUrluescu <urluescualex2003@gmail.com>
2023-04-13 14:34:13 +0300

    mainJS: Logic in JS file

--- html/javascript/main.js
+++ html/javascript/main.js
@@ -6,6 +6,19 @@ let btn_decrease = document.getElementById("decrease");
 
 let counter = document.getElementById("counter");
 
+// database
+let db;
+const request = indexedDB.open('data.db', 1)
+
+request.onerror = (event) => {
+    console.log('Database error: ' + event.target.errorCode);
+}
+
+
+request.onsuccess = (event) => {
+    db = event.target.result;
+}
+
 btn_increase.addEventListener("click", () => {
     console.log("increase clicked!");
     number++;
