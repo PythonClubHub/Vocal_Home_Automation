50cac3ecfc56c4b3cecf1330be50cfdbef45ae12
AlexUrluescu <urluescualex2003@gmail.com>
2023-04-14 14:16:52 +0300

    database: Connection with the data.db file

--- src/database.py
+++ src/database.py
@@ -3,28 +3,28 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
 
-connection = sqlite3.connect('C:/Users/uif94707/Documents/Python/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+connection = sqlite3.connect('database/data.db')
 
 # create a cursor object
 c = connection.cursor()
 
 # execute a query to create a new table
-c.execute('''CREATE TABLE IF NOT EXISTS temperature
-             (id INTEGER PRIMARY KEY,
-                date TEXT,
-                hour TEXT, 
-                temeperature INTEGER,
-                humidity INTEGER
-        )''')
+# c.execute('''CREATE TABLE IF NOT EXISTS temperature
+#              (id INTEGER PRIMARY KEY,
+#                 date TEXT,
+#                 hour TEXT, 
+#                 temeperature INTEGER,
+#                 humidity INTEGER
+#         )''')
 
 #insert data into the table
-# c.execute("INSERT INTO temperature (date, hour, temeperature, humidity) VALUES ('23.04.2023','16:10', 17, 8)")
+c.execute("INSERT INTO temperature (date, hour, temeperature, humidity) VALUES ('30.04.2023','12:55', 18, 2)")
 
 # retrieve data from the table
-c.execute("SELECT * FROM temperature")
-row = c.fetchall()
+# c.execute("SELECT * FROM temperature")
+# row = c.fetchall()
 
-logging.debug(row)
+# logging.debug(row)
 
 # commit the transaction
 connection.commit()
