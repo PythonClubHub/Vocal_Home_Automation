1ad3b1e6a7346c92cf8b83a612477f3c28011942
AlexUrluescu <urluescualex2003@gmail.com>
2023-04-15 14:00:17 +0300

    app.py: Create the router wich update the temp
    This route will let the user update the temperature in the database

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import logging
 import sqlite3
 
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route('/', methods = ['GET'])
 def home():
@@ -22,6 +23,23 @@ def home():
     connection.close()
 
     return jsonify(rows)
+
+
+@app.route('/update_temperature', methods = ['POST'])
+def change_temp():
+    temperature = request.json.get('temperature')
+    connection = sqlite3.connect('C:/Users/Alex/OneDrive/Documente/PythonClubRepos/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+
+    cursor = connection.cursor()
+
+    cursor.execute(f'''
+        UPDATE temperature SET set_temperature = {temperature} WHERE local_home = 'home'
+    ''')
+
+    connection.commit()
+    connection.close()
+
+    return jsonify({'message': 'Temperature updated successfully'})
 
 
 if __name__ == '__main__':
