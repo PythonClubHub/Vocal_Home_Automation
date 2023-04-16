350a47a27a45eb4d6525d381592e19a211178c5e
Timothee <timotei.sandru2022@gmail.com>
2023-04-17 01:02:04 +0300

    Change reading structure

--- src/dht22.py
+++ src/dht22.py
@@ -3,22 +3,20 @@ import Adafruit_DHT
 
 
 class dht_sensor:
-    def __init__(self, temp, humid):
-        self.temp = temp
-        self.humid = humid
+    def __init__(self):
         self.dht_sensor22 = Adafruit_DHT.DHT22
-        self.dht_pin = 4
+        self.dht_pin = 21
 
     def get_t(self):
-        temperature = Adafruit_DHT.read_retry(self.dht_sensor22, self.dht_pin)
-        if temperature is not None:
-            return int(round(temperature, 2))
+        humidity, temperature = Adafruit_DHT.read_retry(self.dht_sensor22, self.dht_pin)
+        if humidity is not None and temperature is not None:
+            return str(round(temperature, 2))
         else:
-            print("Failed to retreive data from humidity sensor")
+            print("Failed to retreive data from temperature sensor")
 
     def get_h(self):
-        humidity= Adafruit_DHT.read_retry(self.dht_sensor22, self.dht_pin)
-        if humidity is not None:
+        humidity, temperature= Adafruit_DHT.read_retry(self.dht_sensor22, self.dht_pin)
+        if humidity is not None and temperature is not None:
             return int(round(humidity, 2))
         else:
             print("Failed to retreive data from humidity sensor")
