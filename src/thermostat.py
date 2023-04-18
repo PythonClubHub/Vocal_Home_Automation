023080fd2b7f4677d5f33800b2b9b0af9141a59c
Timotei Sandru <timotei.sandru2022@gmail.com>
2023-04-18 16:50:57 +0300

    Add funcionality to Thermostat class func

--- src/thermostat.py
+++ src/thermostat.py
@@ -2,10 +2,10 @@
 
 class Thermostat():
     def __init__(self):
-        ...
+        self.threshold_temperature = 19
 
     def get_temperature_threshold(self):
-        ...
+        return self.threshold_temperature
 
-    def set_temperature_threshold(self):
-        ...
+    def set_temperature_threshold(self, new_threshold_temperature):
+        self.threshold_temperature = new_threshold_temperature
