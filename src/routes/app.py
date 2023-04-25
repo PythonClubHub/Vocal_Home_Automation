9f33bbb18da74d477d9420c712f896218642bb87
AlexUrluescu <urluescualex2003@gmail.com>
2023-04-25 11:10:02 +0300

    Moving files

---
+++ src/routes/app.py
@@ -0,0 +1,153 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import logging
+import sqlite3
+from datetime import date
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/', methods = ['GET'])
+def home():
+    # return jsonify({'message': 'home'})
+    connection = sqlite3.connect('C:/Users/uif94707/Documents/Python/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+    # connection = sqlite3.connect('C:/Users/Alex/OneDrive/Documente/PythonClubRepos/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+
+    cursor = connection.cursor()
+
+    cursor.execute('''
+        SELECT * FROM day_average
+    ''')
+
+    rows = cursor.fetchall()
+
+    connection.close()
+
+    return jsonify(rows)
+
+@app.route('/data', methods = ['GET'])
+def data():
+    # return jsonify({'message': 'home'})
+    connection = sqlite3.connect('C:/Users/uif94707/Documents/Python/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+    # connection = sqlite3.connect('C:/Users/Alex/OneDrive/Documente/PythonClubRepos/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+
+    cursor = connection.cursor()
+
+    cursor.execute('''
+        SELECT * FROM temp_table
+    ''')
+
+    rows = cursor.fetchall()
+
+    connection.close()
+
+    return jsonify(rows)
+
+
+@app.route('/update_temperature', methods = ['POST'])
+def change_temp():
+    temperature = request.json.get('temperature')
+    # connection = sqlite3.connect('C:/Users/Alex/OneDrive/Documente/PythonClubRepos/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+    connection = sqlite3.connect('C:/Users/uif94707/Documents/Python/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+
+    cursor = connection.cursor()
+
+    cursor.execute(f'''
+        UPDATE temp_table SET temperature = {temperature} WHERE id = 1
+    ''')
+
+    connection.commit()
+    connection.close()
+
+    return jsonify({'message': 'Temperature updated successfully'})
+
+
+@app.route('/turn_on', methods = ['POST'])
+def turn_on():
+    status = request.json.get('on')
+    # connection = sqlite3.connect('C:/Users/Alex/OneDrive/Documente/PythonClubRepos/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+    connection = sqlite3.connect('C:/Users/uif94707/Documents/Python/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+
+    cursor = connection.cursor()
+
+    cursor.execute(f'''
+        UPDATE temp_table SET status = {status} WHERE id = 1
+    ''')
+
+    connection.commit()
+    connection.close()
+
+    return jsonify({'message': 'Turn On successfully'})
+
+@app.route('/turn_off', methods = ['POST'])
+def turn_off():
+    status = request.json.get('off')
+    # connection = sqlite3.connect('C:/Users/Alex/OneDrive/Documente/PythonClubRepos/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+    connection = sqlite3.connect('C:/Users/uif94707/Documents/Python/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+
+    cursor = connection.cursor()
+
+    cursor.execute(f'''
+        UPDATE temp_table SET status = {status} WHERE id = 1
+    ''')
+
+    connection.commit()
+    connection.close()
+
+    return jsonify({'message': 'Turn On successfully'})
+
+
+
+@app.route('/day_data', methods = ['GET'])
+def date_temp():
+    today = date.today()
+    # current_date = today.strftime("%d.%m.%Y")
+    current_date = '25.04.2023'
+    logging.debug(f"current date = {current_date}")
+
+    # connection = sqlite3.connect('C:/Users/Alex/OneDrive/Documente/PythonClubRepos/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+    connection = sqlite3.connect('C:/Users/uif94707/Documents/Python/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT date, temperature, humidity FROM temperature WHERE date = ?", (current_date,)
+    )
+
+    rows = cursor.fetchall()
+
+    connection.close()
+    return jsonify(rows)
+
+
+@app.route('/week_data', methods = ['GET'])
+def week_data():
+
+    # connection = sqlite3.connect('C:/Users/Alex/OneDrive/Documente/PythonClubRepos/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+    connection = sqlite3.connect('C:/Users/uif94707/Documents/Python/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT avg_temperature, avg_humidity FROM day_average")
+
+    rows = cursor.fetchall()
+
+    connection.close()
+    return jsonify(rows)
+
+
+@app.route('/temperature', methods = ['GET'])
+def temp_day():
+
+    # connection = sqlite3.connect('C:/Users/Alex/OneDrive/Documente/PythonClubRepos/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+    connection = sqlite3.connect('C:/Users/uif94707/Documents/Python/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT date, hour, temperature, humidity FROM temperature")
+
+    rows = cursor.fetchall()
+
+    connection.close()
+    return jsonify(rows)
+
---
+++ src/routes/database.py
@@ -0,0 +1,76 @@
+import sqlite3
+import logging
+from datetime import datetime, date
+import random
+import threading
+import time
+
+# from btn_test import humidiy, tem
+# import sensor
+
+# senzor = sensor.dht_sensor()
+
+logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
+
+def insert_data():
+
+    # get a random temperature and humidity --------------
+    random_temp = random.randint(0, 40)
+    # random_temp = senzor.get_t()
+    logging.debug(f"random temp = {random_temp}")
+
+    random_humidity = random.randint(50, 100)
+    # random_humidity = senzor.get_h()
+    logging.debug(f"random humidity = {random_humidity}")
+
+    # get a random time and date ------------------------------
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
+    logging.debug(f"current time = {current_time}")
+
+    today = date.today()
+    # current_date = today.strftime("%d.%m.%Y")
+    current_date = '01.05.2023'
+    logging.debug(f"current date = {current_date}")
+
+    # connection = sqlite3.connect('C:/Users/Alex/OneDrive/Documente/PythonClubRepos/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+    # connection = sqlite3.connect('C:/Users/uif94707/Documents/Python/Vocal_Home_Automation/Vocal_Home_Automation/database/data.db')
+    connection = sqlite3.connect("C:/Users/uif94707/Documents/Python/Vocal_Home_Automation/Vocal_Home_Automation/src/routes/data.db")
+
+
+    # create a cursor object
+    c = connection.cursor()
+
+    # execute a query to create a new table
+    # c.execute('''CREATE TABLE IF NOT EXISTS temperature
+    #              (id INTEGER PRIMARY KEY,
+    #                 date TEXT,
+    #                 hour TEXT, 
+    #                 temperature INTEGER,
+    #                 humidity INTEGER
+    #         )''')
+
+    #insert data into the table
+    c.execute("INSERT INTO temperature (date, hour, temperature, humidity) VALUES (?, ? , ?, ?)", (current_date, current_time, random_temp, random_humidity))
+
+    # c.execute("DELETE FROM temperature WHERE id >= 84 AND id <= 85")
+    # c.execute("DELETE FROM temperature")
+    # retrieve data from the table
+    # c.execute("SELECT * FROM temperature")
+    # row = c.fetchall()
+
+    # logging.debug(row)
+
+    # commit the transaction
+    connection.commit()
+
+    # close the connectionection
+    connection.close()
+
+
+def timer_insert():
+    while True:
+        insert_data()
+        time.sleep(2)
+
+timer_insert()
